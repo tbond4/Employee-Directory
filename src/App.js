529ab33ebe82d9c08a40bcabe66e42456d22d557
Tristan Bond <tristanebond@gmail.com>
2021-02-28 13:11:59 -0800

    initial state set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Body from "./components/bodyContent";
 import Header from "./components/header";
+import React from "react";
 
+class App extends React.Component {
 
-function App() {
+state= {
+  users:[]
+}
+
+  render(){
   return (
     <div className="App">
      <Header/>
-     <Body/>
+     <Body users={this.state.users}/>
     </div>
-  );
+  )}
 }
 
 export default App;
--- src/components/BodyContent.js
+++ src/components/BodyContent.js
@@ -1,6 +1,8 @@
-function Body(){
+function Body({users}){
     return(
-        
+        <div>
+            {users.map(user => <p> {user.name.first}{user.name.last}</p>)}
+        </div>
     )
     }
     export default Body;
